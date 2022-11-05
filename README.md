14982b1aed1fa7405565bd7adfe268438c098fa1
Yang MinYoung <mazy02@naver.com>
2022-11-05 18:52:42 +0900

    connect react

--- README.md
+++
@@ -1,2 +0,0 @@
-# Alpaco_React_TeamProject
-알파코 React 팀프로젝트
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Alpaco React TeamProject</h1>
+      <h2>팀원</h2>
+      <p>경지혜, 문창일, 박문수, 양민영</p>
+    </div>
+  );
+}
+
+export default App;
