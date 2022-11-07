9b3c1e0af7e7b7bf72fc96320aa76d69f41dc48b
Kyung JiHye <k92270217@gmail.com>
2022-11-08 08:52:28 +0900

    setting file update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Alpaco React TeamProject</h1>
+      <h1 className='bg-emerald-400'>Alpaco React TeamProject</h1>
       <h2>팀원</h2>
       <p>경지혜, 문창일, 박문수, 양민영</p>
     </div>
